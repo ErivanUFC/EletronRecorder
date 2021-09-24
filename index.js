0b94bf28a9bac62800b556f9ac21a49247a288bd
ErivanUFC <erivan@alu.ufc.br>
2021-09-23 21:01:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+let camera_button = document.querySelector("#start-camera");
+let video = document.querySelector("#video");
+let start_button = document.querySelector("#start-record");
+let stop_button = document.querySelector("#stop-record");
+let download_link = document.querySelector("#download-video");
+
+let camera_stream = null;
+let media_recorder = null;
+let blobs_recorded = [];
+
+camera_button.addEventListener('click', async function () {
+  camera_stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  video.srcObject = camera_stream;
+});
+
+start_button.addEventListener('click', function () {
+  // set MIME type of recording as video/webm
+  media_recorder = new MediaRecorder(camera_stream, { mimeType: 'video/webm' });
+
+  // event : new recorded video blob available 
+  media_recorder.addEventListener('dataavailable', function (e) {
+    blobs_recorded.push(e.data);
+  });
+
+  // event : recording stopped & all blobs sent
+  media_recorder.addEventListener('stop', function () {
+    // create local object URL from the recorded video blobs
+    // let video_local = URL.createObjectURL(new Blob(blobs_recorded, { type: 'video/webm' }));
+    // download_link.href = video_local;
+    // download_link.click();
+
+    const blob = new Blob(blobs_recorded, { type: 'video/webm' });
+    const buffer = Buffer.from( blob.arrayBuffer() );
+    console.log(buffer)
+    // fs.writeFile('video.webm', buffer, () => console.log('video saved!'));
+  });
+
+  // start recording with each recorded blob having 1 second video
+  media_recorder.start(1000);
+
+  var counterNumber = 0
+  setInterval(
+    function () {
+      counterNumber += 1;
+      counter.innerHTML = counterNumber;
+
+      if (counterNumber == 5) {
+        media_recorder.stop();
+      }
+    }, 1000);
+});
+
+// stop_button.addEventListener('click', function () {
+//  media_recorder.stop();
+// });
